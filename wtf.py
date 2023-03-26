78745f5b8dcab78f281d5b6e96ff91dc4451d572
Mohammad Naeem <inaeem@protonmail.com>
2023-03-26 11:11:43 +0500

    Add wtform to template

--- wtf.py
+++ wtf.py
@@ -3,12 +3,13 @@ from forms import RegistrationForm
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2cedbf5ee35939c74d7ea22c58dc4aba5793facaebdef3153de60de738dd3c5a'
 
 
 @app.route('/')
 def index():
-
-    return render_template('index.html', )
+    form = RegistrationForm()
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
