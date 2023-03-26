453eedcf110fffdf0bfb5af19e8454a5545e8469
Mohammad Naeem <inaeem@protonmail.com>
2023-03-26 11:12:01 +0500

    Add wtforms validtors

--- forms.py
+++ forms.py
@@ -1,10 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField("Email")
-    username = StringField("Username")
-    password = StringField("Password")
+    email = StringField("Email",
+                        validators=[DataRequired(), Length(min=2, max=25)])
+    username = StringField("Username",
+                           validators=[DataRequired(), Length(min=2, max=25)])
+    password = StringField("Password",
+                           validators=[DataRequired(), Length(min=2, max=25)])
 
     submit = SubmitField("Sign up")
