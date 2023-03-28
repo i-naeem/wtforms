44a2cdc752af58b24ca5ed081bee2ba753008f7a
Mohammad Naeem <inaeem@protonmail.com>
2023-03-28 11:44:27 +0500

    add post method

--- wtf.py
+++ wtf.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from forms import RegistrationForm
 
 
@@ -6,8 +6,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '2cedbf5ee35939c74d7ea22c58dc4aba5793facaebdef3153de60de738dd3c5a'
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == 'POST':
+        return request.form.to_dict()
     form = RegistrationForm()
     return render_template('index.html', form=form)
 
