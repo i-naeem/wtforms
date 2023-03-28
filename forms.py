aa4dbd3ada14ce5590a008da6756e31f9a662e95
Mohammad Naeem <inaeem@protonmail.com>
2023-03-28 11:39:00 +0500

    Add agree to terms input

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.fields import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, InputRequired
 
 
 class RegistrationForm(FlaskForm):
@@ -10,5 +10,8 @@ class RegistrationForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=25)])
     password = StringField("Password",
                            validators=[DataRequired(), Length(min=2, max=25)])
+
+    agreement = BooleanField("I agree to terms and condition",
+                             validators=[InputRequired()])
 
     submit = SubmitField("Sign up")
